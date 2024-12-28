edeae2f3286491e6087b0e57c702f1b9316b4367
Shanaka Prince <shanakaprince@gmail.com>
2024-12-28 10:26:06 +0530

    ChangePassword

--- src/test/java/stepDefinitions/BuzzStepDefinitions.java
+++ src/test/java/stepDefinitions/BuzzStepDefinitions.java
@@ -1,6 +1,7 @@
 package stepDefinitions;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -187,8 +188,7 @@ public class BuzzStepDefinitions {
 	    }
 	}
 
+}
 
-}
-}
 
 
