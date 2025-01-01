ebd7fd23e5f74eda39a826334799e679b9e33cb7
bhojasuriyaguptha <bhojasuriyaguptha1234@gmail.com>
2025-01-01 08:21:22 -0800

    update

--- src/test/java/stepDefinitions/TestRunner.java
+++ src/test/java/stepDefinitions/TestRunner.java
@@ -11,8 +11,7 @@ import org.testng.annotations.DataProvider;
                 "pretty",
                 "json:target/cucumber.json",
                 "html:target/cucumber-reports/cucumber.html"
-        },tags = "@LoginPageURLCheck or @Login or @SaveHoliday"
-        //tags = "@LoginPageURLCheck or @Login or @ChangePassword or @BuzzComplete or  @SearchReport or @LogoutCheck or @InputValidationMyInfo or @SaveHoliday"
+        }, tags = "@LoginPageURLCheck or @Login or @ChangePassword or @BuzzComplete or  @SearchReport or @LogoutCheck or @InputValidationMyInfo or @SaveHoliday"
 
 
 )
